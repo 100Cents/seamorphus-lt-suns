641ccc66c20b3cb27ecb256dad00e1d326aa1519
100Cents <p.scalia@gmail.com>
2022-06-07 09:54:39 +0200

    Add files via upload

--- hashlips_nft_minting_dapp-main/src/App.js
+++ hashlips_nft_minting_dapp-main/src/App.js
@@ -206,7 +206,7 @@ function App() {
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
-            <StyledImg alt={"example"} src={"/config/images/example.gif"} />
+            <StyledImg alt={"example"} src={"/config/images/sun.png"} />
           </s.Container>
           <s.SpacerLarge />
           <s.Container
@@ -374,7 +374,7 @@ function App() {
           <s.Container flex={1} jc={"center"} ai={"center"}>
             <StyledImg
               alt={"example"}
-              src={"/config/images/example.gif"}
+              src={"/config/images/sun.png"}
               style={{ transform: "scaleX(-1)" }}
             />
           </s.Container>
